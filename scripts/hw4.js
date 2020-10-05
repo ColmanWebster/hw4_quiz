77309a2c9046ea1fac50310f62c429a00fcfbb94
Colman Webster <roguefiend@yahoo.com>
2020-10-04 22:34:10 -0700

    code bulk

--- scripts/hw4.js
+++ scripts/hw4.js
@@ -1,28 +1,109 @@
-/* VARIABLES */
-var start   = document.getElementById("start");
-var display = document.getElementById("display");
-    /* quiz questions */
-    /* quiz answers */     /* possibly an array within an array? */
 
-/* FUNCTIONS */
+              /* [0]  [1]  [2] */
+var answerKey = ['4', '6', '11'];
 
-    /* timer function */
-    /* increment counter? */
+var question1 = "question one?";          /* index [0] */
+var question2 = "question two?";          /* index [1] */
+var question3 = "question three?";        /* index [2] */
 
-/* SCRIPT */
+var questionsList = [question1, question2, question3];
 
-    start.addEventListener('click', Timer);
+var answers1 = ['1', '2', '3', '4'];      /* index [0] */
+var answers2 = ['5', '6', '7', '8'];      /* index [1] */
+var answers3 = ['9', '10', '11', '12'];   /* index [2] */
 
-    var int;
+var answersList = [answers1, answers2, answers3];
 
-    function Timer(){
-        int = setInterval(Time1, 2000);
-        start.remove();
- 
+var cont3 = document.querySelector('#cont3');
+var display = document.querySelector('#display');
+var prompts = document.querySelector('#prompts');
+var start =   document.querySelector('#start');
+var press = 0;
+var int;
+var pausetime;
+var correct = 0;
+
+pausetime = 120;
+prompts.value = pausetime;
+
+start.addEventListener('click', function(event){
+    if (event.target.matches('#start') && press == 0){
+        
+        function Timer(){
+            
+            prompts.value = pausetime;
+            int = setInterval(Time1, 1000);
+            
+        };
+        function Time1(){
+            if (prompts.value > 0){
+            prompts.value -= 1;          
+        } else{
+            clearInterval(int);
+            pausetime = 10;
+            prompts.value = pausetime;
+            start.textContent = "START";
+            press = 0;
+        }
+        };
+        Timer();
+        
+        press = 1;
+        
+        start.textContent = "PAUSE";  
+    } else if (event.target.matches('#start') && press == 1){
+        clearInterval(int);
+        pausetime = prompts.value;
+        prompts.value = "PAUSED: " + pausetime;
+        press = 0;
+        
+        start.textContent = "START";
+    } else if (pausetime >= 0){
+        clearInterval(int);
     }
-    function Time1(){
-        display.value = "Two-Second Timer";    
-        clearInterval(int);      
-    }
+});
 
+/* display.value = answersList[1][2]; */
+
+var a = 0;
+var b = 0;
+
+start.addEventListener('click', function(){
     
+    display.value = questionsList[b];
+    Loop();
+    
+    cont3.addEventListener('click', function(event){
+
+    if (event.target.matches('.button') && event.target.textContent == answerKey[b]){
+        alert('CORRECT');
+        if (b == 3){
+            b = 0;
+        }
+        b++;
+        correct ++;
+
+
+        if (pausetime == 0 || correct == 3){
+
+            document.write('congration you done it');
+        }
+
+
+        display.value = questionsList[b];
+        Loop();
+    }
+});
+
+});
+
+function Loop(){
+
+    for (j = 0; j < answersList[b].length; j++){
+        document.getElementsByClassName('button')[j].textContent = answersList[b][j];
+
+        
+    
+    }
+}
+
